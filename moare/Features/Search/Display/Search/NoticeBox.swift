7510104ab5301115026513c05ca8d98c36b7369e
bestallround <ymb3264@gmail.com>
2025-03-17 18:31:20 +0900

    improvement: Update notice box

--- moare/Features/Search/Display/Search/NoticeBox.swift
+++ moare/Features/Search/Display/Search/NoticeBox.swift
@@ -14,13 +14,18 @@ struct NoticeBox: View {
                 Text("현재 제공중인 스포츠 데이터:")
                     .font(.system(size: 12))
                     .foregroundStyle(.secondary)
-                Text("• 프리미어리그 24/25")
+                Text("• 프리미어리그 24/25" +
+                     "\n• 라리가 24/25" +
+                     "\n• 분데스리가 24/25" +
+                     "\n• 리그 1 24/25")
                     .font(.system(size: 12))
                     .foregroundStyle(.secondary)
                 Text("\n제공 예정 스포츠 데이터:")
                     .font(.system(size: 12))
                     .foregroundStyle(.secondary)
-                Text("• 라리가 24/25\n• 분데스리가 24/25\n• 리그 1 24/25\n• 챔피언스리그 24/25")
+                Text("• 챔피언스리그 24/25" +
+                     "\n• KBO 리그 2025" +
+                     "\n• MLB 2025")
                     .font(.system(size: 12))
                     .foregroundStyle(.secondary)
             }
