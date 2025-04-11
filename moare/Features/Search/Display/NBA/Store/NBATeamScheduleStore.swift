52ae397250d056db8540045e9ae537131bbd2521
bestallround <ymb3264@gmail.com>
2025-04-11 22:19:45 +0900

    feature: Create NBATeamScheduleStore

--- moare/Features/Search/Display/NBA/Store/NBATeamScheduleStore.swift
+++ moare/Features/Search/Display/NBA/Store/NBATeamScheduleStore.swift
@@ -17,20 +17,55 @@ struct NBATeamScheduleStore {
            data state
            --------------------- */
         var displayModel: NBATeamScheduleDisplayModel? = nil
+        var games: [NBAGame] = []
+        
+        /* ---------------------
+           ui state
+           --------------------- */
+        var isAllResultOpened = false
+        var gameResultOpenedStateList: [String: Bool] = [:]
     }
     
     enum Action {
+        /* ---------------------
+           init
+           --------------------- */
         case initData(displayModel: NBATeamScheduleDisplayModel)
+        
+        /* ---------------------
+           view action
+           --------------------- */
+        case toggleAllResult
+        case updateResultOpenedState(gameCode: String, isOpened: Bool)
     }
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .initData(let displayModel):
+                // init with default value
+                state.isAllResultOpened = false
+                
+                // init data
                 state.displayModel = displayModel
+                state.games = displayModel.games
+                
+                let gameResultOpenedStateList = (state.games).reduce(into: [:]) { $0[$1.gameSummary?.gameCode ?? ""] = false }
+                state.gameResultOpenedStateList = gameResultOpenedStateList
                 
                 return .none
-            }
+                
+            case .toggleAllResult:
+                let newState = !state.isAllResultOpened
+                state.isAllResultOpened = newState
+                state.gameResultOpenedStateList = state.gameResultOpenedStateList.mapValues { _ in newState }
+                return .none
+                
+            case .updateResultOpenedState(let gameCode, let isOpened):
+                state.gameResultOpenedStateList[gameCode] = isOpened
+                
+                return .none
+            } // switch action
         }
     }
 }
