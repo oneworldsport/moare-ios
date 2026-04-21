1172559c7e459262a5cf448d4710df7ca77aba9f
최지혜 <jhchoi@joara.com>
2026-04-21 14:15:26 +0900

    improvement: Remove property 'inningsPitched' in model KBOGamePitcherStats

--- moare/Features/Search/Display/KBO/View/KBOGameStatsView.swift
+++ moare/Features/Search/Display/KBO/View/KBOGameStatsView.swift
@@ -55,7 +55,7 @@ struct KBOGameStatsView: View {
             StandingsItemState(
                 imageUrl: KBOUtil.playerPhotoURL(season: displayModel.season, id: $0.id),
                 name: $0.name,
-                dataList: [String($0.inningsPitched), $0.r, $0.er, $0.bb, $0.so, $0.h]
+                dataList: [$0.ip, $0.r, $0.er, $0.bb, $0.so, $0.h]
             )
         }
         
